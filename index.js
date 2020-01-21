8587cb9112dedf1a0edafbb5a804ff0c78491869
Krrish Mittal <takyyon@Krrishs-MacBook-Pro-2.local>
2020-01-21 15:15:50 -0800

    index.js updated

--- index.js
+++ index.js
@@ -7,15 +7,6 @@ const app = express();
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    const path = require('path');
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    });
-}
-
 const platformController = require('./server/controller/platform.controller.server');
 platformController(app);
 
@@ -33,6 +24,14 @@ appController(app);
 
 const featureController = require('./server/controller/feature.controller.server');
 featureController(app);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    const path = require('path');
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
