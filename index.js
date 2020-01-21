45802ca43f65f9b4929012ef5b3f76025ff10d35
Krrish Mittal <takyyon@Krrishs-MacBook-Pro-2.local>
2020-01-21 15:52:55 -0800

    update 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require("path")
 require('./server/model/db');
 
 const app = express();
@@ -34,10 +35,9 @@ const featureController = require('./server/controller/feature.controller.server
 featureController(app);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    const path = require('path');
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    app.use(express.static(path.join(__dirname, "client", "build")))
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "client", "build", "index.html"));
     });
 }
 
