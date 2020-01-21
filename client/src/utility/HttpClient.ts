382de838c9b87613d2fa6961f1d7cdb9a4036777
Krrish Mittal <takyyon@Krrishs-MacBook-Pro-2.local>
2020-01-21 15:10:01 -0800

    url changed

--- client/src/utility/HttpClient.ts
+++ client/src/utility/HttpClient.ts
@@ -1,9 +1,9 @@
 import axios, { Method } from 'axios';
 
 export const makeRequest = (url: string, method: Method, parameters?: any, body?:any) => {
-    if(!url.startsWith('http://') && !url.startsWith('https://')) {
-        url = `http://localhost:5000/${url}`;
-    }
+    // if(!url.startsWith('http://') && !url.startsWith('https://')) {
+    //     url = `http://localhost:5000/${url}`;
+    // }
     return axios.request({
         url: url,
         method: method,
