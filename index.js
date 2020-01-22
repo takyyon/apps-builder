e0c6e5a63674ebb5d17219e67d61fc7fe1b4a954
Krrish Mittal <takyyon@Krrishs-MacBook-Pro-2.local>
2020-01-21 16:25:38 -0800

    index.js updated

--- index.js
+++ index.js
@@ -34,12 +34,12 @@ appController(app);
 const featureController = require('./server/controller/feature.controller.server');
 featureController(app);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, "client", "build")))
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-    });
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, "client", "build")))
+//     app.get("*", (req, res) => {
+//       res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+//     });
+// }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
