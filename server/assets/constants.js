f09e5bec1a6597e39eb0c528a7a57295f3460c91
Krrish Mittal <takyyon@Krrishs-MacBook-Pro-2.local>
2020-01-21 16:41:09 -0800

    monogo url updated

--- server/assets/constants.js
+++ server/assets/constants.js
@@ -1,5 +1,5 @@
 module.exports = () => {
-    const mongodbString = 'mongodb://heroku_f2sk2zpn:da4uibq4dr40v2972mbpgvsdk3@ds135724.mlab.com:35724/heroku_f2sk2zpn';
+    const mongodbString = 'mongodb://heroku_zr1wsjd0:r79jrqu1m8ghu9sq82sso5b5ot@ds141937.mlab.com:41937/heroku_zr1wsjd0';
     return {
         mongodbString,
     }
