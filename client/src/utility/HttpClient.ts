e868164d6b7b036c2117a8893932641853f7ac0f
Krrish Mittal <takyyon@Krrishs-MacBook-Pro-2.local>
2020-01-21 16:13:36 -0800

    axios proxy added

--- client/src/utility/HttpClient.ts
+++ client/src/utility/HttpClient.ts
@@ -1,9 +1,9 @@
 import axios, { Method } from 'axios';
 
+axios.defaults.baseURL = 'http://localhost:5000';
+
 export const makeRequest = (url: string, method: Method, parameters?: any, body?:any) => {
-    if(!url.startsWith('http://') && !url.startsWith('https://')) {
-        url = `http://localhost:5000${url}`;
-    }
+    
     return axios.request({
         url: url,
         method: method,
