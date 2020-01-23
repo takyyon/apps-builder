4333e31872c73fb4c87509b3dc59add6b2131467
Krrish Mittal <takyyon@Krrishs-MacBook-Pro-2.local>
2020-01-22 22:46:04 -0800

    index updated

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ appController(app);
 const featureController = require('./server/controller/feature.controller.server');
 featureController(app);
      
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-    });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
